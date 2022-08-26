f2cb08c29eed7e200767062d4a25e3e9123707f0
Apollo-Roboto <tiralex@hotmail.com>
2022-08-25 22:32:59 -0400

    Generation startup

---
+++ targets/cs/constants/Space.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ApolloRoboto.Constants
+{
+	/// data from https://nssdc.gsfc.nasa.gov/planetary/factsheet/
+	/// distances are in km
+	/// masses when present are in kg
+	public struct Space
+	{
+		
+	}
+}
